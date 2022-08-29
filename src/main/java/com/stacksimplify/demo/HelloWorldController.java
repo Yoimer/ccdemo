fbad50e048764ecc21566bfa44d37656ccdcbaa6
Yoimer <Yoimer@LAPTOP-H5V0V7QG>
2022-08-29 13:33:54 -0400

    First Commit

---
+++ src/main/java/com/stacksimplify/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hi from ccdemo - v1";
+	}
+}
---
+++ src/test/java/com/stacksimplify/demo/CcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
